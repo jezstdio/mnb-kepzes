e72d3da171b1623e2bb4acf9a26c95094b880c71
jezstdio <jezstdio@gmail.com>
2023-02-13 20:05:18 +0100

    now it's measures all the given ansers

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -41,6 +41,7 @@ function Quiz(props) {
 function GameSpace(props) {
     const questions = props.data;
     const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [allAnswers, setAllAnswers] = useState(0);
     const [isAnswerCorrect, setIsAnswerCorrect] = useState(undefined);
     const [isAnimating, setIsAnimating] = useState(false);
 
@@ -106,7 +107,7 @@ function GameSpace(props) {
 
     return (
         <div className={`relative height-100vh width-100 padding-x-24 padding-t-56 padding-b-80 text-center max_width-512px margin-x-auto`}>
-            <span>{correctAnswers}/{questions.length}</span>
+            <span>{correctAnswers} / {allAnswers} / {questions.length}</span>
             <div className={`animating ${isAnimating ? 'animating-on' : ''}`}>
                 <span className="block font_size-24 font_weight-bold margin-b-56">{ questions[0]["Kérdés"] }</span>
                 <div className="padding-b-128">
@@ -114,6 +115,7 @@ function GameSpace(props) {
                         key={`${index}${answer}`}
                         index={index}
                         setCorrectAnswers={setCorrectAnswers}
+                        setAllAnswers={setAllAnswers}
                         isAnswerCorrect={isAnswerCorrect}
                         setIsAnswerCorrect={setIsAnswerCorrect}
                         answer={answer} />) }
@@ -143,9 +145,11 @@ function AnswerButton(props) {
     const button = useRef();
 
     function handleClick() {
+        props.setAllAnswers(count => count + 1);
+        
         if (props.answer[1]) {
             setStatus("correct");
-            props.setCorrectAnswers(count => count + 1)
+            props.setCorrectAnswers(count => count + 1);
             props.setIsAnswerCorrect(true);
         } else {
             setStatus("wrong");
@@ -156,7 +160,6 @@ function AnswerButton(props) {
     function answerKeyDown(e) {
         if (parseInt(e.key) === parseInt(props.index + 1) && !button.current.disabled) {
             handleClick();
-            console.log(e.key, props.index + 1);
         }
     }
 
